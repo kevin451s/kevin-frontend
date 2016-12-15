b83a1226b135368a66a0adb1a7d7e273ff89d440
Kevin Salazar <kskevin170@yahoo.com>
2016-12-15 12:05:02 -0800

    fixed routes and styling

--- js/pictures/pictures.resource.js
+++ js/pictures/pictures.resource.js
@@ -6,6 +6,6 @@
   PictureResource.$inject = ['$resource'];
 
   function PictureResource($resource) {
-    return $resource('http://localhost:3000/pictures/:id', {id: '@_id'}, { 'update': { method: 'PATCH'}})
+    return $resource('http://localhost:3000/api/pictures/:id', {id: '@_id'}, { 'update': { method: 'PATCH'}})
   }
 }());
--- js/services/services.resource.js
+++ js/services/services.resource.js
@@ -6,6 +6,6 @@
   ServiceResource.$inject = ['$resource'];
 
   function ServiceResource($resource) {
-    return $resource('http://localhost:3000/services/:id', {id: '@_id'}, { 'update': { method: 'PATCH'}})
+    return $resource('http://localhost:3000/api/services/:id', {id: '@_id'}, { 'update': { method: 'PATCH'}})
   }
 }());
